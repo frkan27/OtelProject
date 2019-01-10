f36e73345c00921913744afd52380e57d4f97ad5
Furkan Kılıç <furkankilicytu@gmail.com>
2019-01-11 00:34:48 +0300

    kategoriview

--- Otel.WFA/Form1.Designer.cs
+++ Otel.WFA/Form1.Designer.cs
@@ -28,13 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tvCategory = new System.Windows.Forms.TreeView();
+            this.cmbKategori = new System.Windows.Forms.ComboBox();
+            this.txtKategoriAdi = new System.Windows.Forms.TextBox();
+            this.rtbAciklama = new System.Windows.Forms.RichTextBox();
+            this.btnKategori = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tvCategory
+            // 
+            this.tvCategory.FullRowSelect = true;
+            this.tvCategory.Location = new System.Drawing.Point(39, 22);
+            this.tvCategory.Name = "tvCategory";
+            this.tvCategory.Size = new System.Drawing.Size(195, 336);
+            this.tvCategory.TabIndex = 0;
+            // 
+            // cmbKategori
+            // 
+            this.cmbKategori.FormattingEnabled = true;
+            this.cmbKategori.Location = new System.Drawing.Point(253, 22);
+            this.cmbKategori.Name = "cmbKategori";
+            this.cmbKategori.Size = new System.Drawing.Size(145, 21);
+            this.cmbKategori.TabIndex = 1;
+            // 
+            // txtKategoriAdi
+            // 
+            this.txtKategoriAdi.Location = new System.Drawing.Point(253, 66);
+            this.txtKategoriAdi.Name = "txtKategoriAdi";
+            this.txtKategoriAdi.Size = new System.Drawing.Size(145, 20);
+            this.txtKategoriAdi.TabIndex = 2;
+            // 
+            // rtbAciklama
+            // 
+            this.rtbAciklama.Location = new System.Drawing.Point(253, 110);
+            this.rtbAciklama.Name = "rtbAciklama";
+            this.rtbAciklama.Size = new System.Drawing.Size(145, 93);
+            this.rtbAciklama.TabIndex = 3;
+            this.rtbAciklama.Text = "";
+            // 
+            // btnKategori
+            // 
+            this.btnKategori.Location = new System.Drawing.Point(253, 236);
+            this.btnKategori.Name = "btnKategori";
+            this.btnKategori.Size = new System.Drawing.Size(145, 23);
+            this.btnKategori.TabIndex = 4;
+            this.btnKategori.Text = "Kaydet";
+            this.btnKategori.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnKategori);
+            this.Controls.Add(this.rtbAciklama);
+            this.Controls.Add(this.txtKategoriAdi);
+            this.Controls.Add(this.cmbKategori);
+            this.Controls.Add(this.tvCategory);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView tvCategory;
+        private System.Windows.Forms.ComboBox cmbKategori;
+        private System.Windows.Forms.TextBox txtKategoriAdi;
+        private System.Windows.Forms.RichTextBox rtbAciklama;
+        private System.Windows.Forms.Button btnKategori;
     }
 }
 
